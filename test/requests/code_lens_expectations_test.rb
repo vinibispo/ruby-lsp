c243dafe38a8bb15a54f09e278094afcdfb02096
Thomas Marshall <thomas.marshall@shopify.com>
2024-10-09 21:23:26 +0100

    Set load path based on test/spec directory
    Some codebases (particularly those using minitest/spec) have tests in
    the `spec` directory instead of the `test` directory. This commit adds
    support for detecting these paths and adjusting the load path
    accordingly.

--- test/requests/code_lens_expectations_test.rb
+++ test/requests/code_lens_expectations_test.rb
@@ -29,7 +29,7 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
         def test_bar; end
       end
     RUBY
-    uri = URI("file:///fake.rb")
+    uri = URI("file:///test/fake.rb")
 
     document = RubyLsp::RubyDocument.new(source: source, version: 1, uri: uri)
 
@@ -42,12 +42,12 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
 
     assert_equal("Run In Terminal", T.must(response[1]).command.title)
     assert_equal(
-      "bundle exec ruby -Itest /fake.rb --name \"/^FooTest(#|::)/\"",
+      "bundle exec ruby -Itest /test/fake.rb --name \"/^FooTest(#|::)/\"",
       T.must(response[1]).command.arguments[2],
     )
     assert_equal("Run In Terminal", T.must(response[4]).command.title)
     assert_equal(
-      "bundle exec ruby -Itest /fake.rb --name FooTest#test_bar",
+      "bundle exec ruby -Itest /test/fake.rb --name FooTest#test_bar",
       T.must(response[4]).command.arguments[2],
     )
   end
@@ -61,7 +61,7 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
         end
       end
     RUBY
-    uri = URI("file:///fake.rb")
+    uri = URI("file:///spec/fake.rb")
 
     document = RubyLsp::RubyDocument.new(source: source, version: 1, uri: uri)
 
@@ -74,17 +74,17 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
 
     assert_equal("Run In Terminal", T.must(response[1]).command.title)
     assert_equal(
-      "bundle exec ruby -Itest /fake.rb --name \"/^FooTest(#|::)/\"",
+      "bundle exec ruby -Ispec /spec/fake.rb --name \"/^FooTest(#|::)/\"",
       T.must(response[1]).command.arguments[2],
     )
     assert_equal("Run In Terminal", T.must(response[4]).command.title)
     assert_equal(
-      "bundle exec ruby -Itest /fake.rb --name \"/^a(#|::)/\"",
+      "bundle exec ruby -Ispec /spec/fake.rb --name \"/^a(#|::)/\"",
       T.must(response[4]).command.arguments[2],
     )
     assert_equal("Run In Terminal", T.must(response[7]).command.title)
     assert_equal(
-      "bundle exec ruby -Itest /fake.rb --name \"/^a#test_0001_b$/\"",
+      "bundle exec ruby -Ispec /spec/fake.rb --name \"/^a#test_0001_b$/\"",
       T.must(response[7]).command.arguments[2],
     )
   end
@@ -96,7 +96,7 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
         def test_bar; end
       end
     RUBY
-    uri = URI("file:///fake.rb")
+    uri = URI("file:///test/fake.rb")
 
     document = RubyLsp::RubyDocument.new(source: source, version: 1, uri: uri)
 
@@ -108,10 +108,10 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
     assert_equal(6, response.size)
 
     assert_equal("Run In Terminal", T.must(response[1]).command.title)
-    assert_equal("bundle exec ruby -Itest /fake.rb --testcase /FooTest/", T.must(response[1]).command.arguments[2])
+    assert_equal("bundle exec ruby -Itest /test/fake.rb --testcase /FooTest/", T.must(response[1]).command.arguments[2])
     assert_equal("Run In Terminal", T.must(response[4]).command.title)
     assert_equal(
-      "bundle exec ruby -Itest /fake.rb --testcase /FooTest/ --name test_bar",
+      "bundle exec ruby -Itest /test/fake.rb --testcase /FooTest/ --name test_bar",
       T.must(response[4]).command.arguments[2],
     )
   end
